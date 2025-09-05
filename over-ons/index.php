6e1919cf7dda8901b727e93b89a19b72b8317076
Guled-01 <gmohamed@ictvanafmorgen.nl>
2025-09-05 13:49:13 +0200

    over of oever?!?

--- over-ons/index.php
+++ over-ons/index.php
@@ -2,44 +2,50 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/styling/global.css">
-  <link rel="stylesheet" href="/styling/over-ons.css">
-  <title>Over Ons - Hotel De Zonne Vallei</title>
-  <script src="/over-ons/script.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styling/global.css">
+    <link rel="stylesheet" href="/styling/over-ons.css">
+    <link rel="stylesheet" href="/styling/footer.css">
+    <title>Over-ons - Hotel De Zonne Vallei</title>
+    <link rel="icon" href="/assets/logos/favicon.ico">
 </head>
-
 <body>
-   <?php include('../assets/html/navbar.html'); ?>
-  <section class="cont">
-    
-    <h1>Over ons</h1>
-    <section class="box">
 
-      <main class="box2">
-        <h2 class="h2">Welkom bij Hotel De Zonne Vallei</h2>
-        <p>Bij Hotel De Zonne Vallei streven we ernaar om elke gast een onvergetelijke ervaring te bieden, doordrenkt met luxe, comfort en uitzonderlijke gastvrijheid. Gelegen in het hart van Alkmaar, biedt ons hotel de perfecte uitvalsbasis om deze historische en charmante stad te ontdekken.</p>
-       <h2 class="h2">Ons Verhaal</h2>
+<?php include("../assets/html/navbar.html"); ?>
+
+    <div class="hero-container">
+        <div class="hero-content-wrapper">
+            <h1 id="dark">Welkom bij Hotel De Zonne Vallei</h1>
+            <p id="dark2">Bij Hotel De Zonne Vallei streven we ernaar om elke gast een onvergetelijke ervaring te bieden, doordrenkt met luxe, comfort en uitzonderlijke gastvrijheid. Gelegen in het hart van Alkmaar, biedt ons hotel de perfecte uitvalsbasis om deze historische en charmante stad te ontdekken.</p>  <!-- tesk opmaak verbetern-->
+            
+        </div>
+    </div>
+
+    <section class="about-section">
+        <div class="about-content-wrapper">
+           <h2 class="h2">Ons Verhaal</h2>
        <p>Hotel De Zonne Vallei is opgericht door Bouke van Zon, een visionaire ondernemer met een passie voor gastvrijheid en een scherp oog voor detail. Met jarenlange ervaring in de horeca-industrie heeft Bouke zijn droom verwezenlijkt om een uniek hotel te creëren waar gasten zich thuis voelen en kunnen genieten van alle gemakken en luxe die het leven te bieden heeft.</P>
-        <h2 class="h2">Onze Waarden</h2>
-       <p>Bij Hotel De Zonne Vallei staan kwaliteit, gastvrijheid en persoonlijke service centraal. Wij geloven dat elk detail bijdraagt aan een perfecte ervaring, van de smaakvolle inrichting van onze kamers tot de culinaire hoogstandjes in ons restaurant. Ons toegewijde team staat altijd klaar om aan al uw wensen te voldoen en ervoor te zorgen dat uw verblijf zo aangenaam mogelijk is.</P>
-        <h2 class="h2">Ons Team</h2>
-       <p>Ons team van enthousiaste en professionele medewerkers deelt de visie van Bouke van Zon om elke gast een warm welkom en een onvergetelijk verblijf te bieden. Van de receptie tot het restaurant en de huishouding, elk teamlid speelt een cruciale rol in het creëren van de unieke sfeer en ervaring waar Hotel De Zonne Vallei om bekend staat.</P>
-      </main>
-      <main class="box3">
-
-        <article class="w3-content w3-section" style="max-width:500px">
-          <img class="mySlides" src="https://wallpaperaccess.com/full/658385.jpg" alt="hotel foto">
-          <img class="mySlides" src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/619990272.jpg?k=0cdca0c64117ed139eaeb5198076a2aa715ec9e6ab121fd687d438d93d832399&o=&hp=1" alt="kamer foto">
-          <img class="mySlides" src="https://fastly.picsum.photos/id/42/3456/2304.jpg?hmac=dhQvd1Qp19zg26MEwYMnfz34eLnGv8meGk_lFNAJR3g" alt="restaurant foto">
-        </article>
-      </main>
-      
+        </div>
     </section>
-  </section>
-  <?php include('../assets/html/footer.html'); ?>
-    
+
+    <section class="about-section2" id="menu">
+        <div class="about-content-wrapper2">
+            
+             <h2 class="h2">Onze Waarden</h2>
+       <p>Bij Hotel De Zonne Vallei staan kwaliteit, gastvrijheid en persoonlijke service centraal. Wij geloven dat elk detail bijdraagt aan een perfecte ervaring, van de smaakvolle inrichting van onze kamers tot de culinaire hoogstandjes in ons restaurant. Ons toegewijde team staat altijd klaar om aan al uw wensen te voldoen en ervoor te zorgen dat uw verblijf zo aangenaam mogelijk is.</P>
+        </div>
+    </section>
+
+    <section class="about-section" id="reserveren">
+        <div class="about-content-wrapper">
+          <h2 class="h2">Ons Team</h2>
+       <p>Ons team van enthousiaste en professionele medewerkers deelt de visie van Bouke van Zon om elke gast een warm welkom en een onvergetelijk verblijf te bieden. Van de receptie tot het restaurant en de huishouding, elk teamlid speelt een cruciale rol in het creëren van de unieke sfeer en ervaring waar Hotel De Zonne Vallei om bekend staat.</P>
+        </div>
+    </section>
+
+<?php include("../assets/html/footer.html"); ?>
+
 </body>
 
 </html>
